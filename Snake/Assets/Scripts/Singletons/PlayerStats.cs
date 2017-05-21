c6ce847b6d5c8fdf789a41b916f5a334ad76c2eb
Zarkwon <adamlis05@gmail.com>
2017-05-21 11:46:21 -0300

    Snake initial commit

---
+++ Snake/Assets/Scripts/Singletons/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : Singleton<PlayerStats> {
+	// this should just be a JSON doc from dynamo
+	public int level {get; set;}
+	public int highestScore {get; set;}
+
+	protected PlayerStats() {
+		ResetStats ();
+	}
+		
+	public void LoadStats() {
+		Debug.Log ("LoadStats");
+	}
+
+	public void ResetStats() {
+		level = 0;
+		highestScore = 0;
+	}
+}
---
+++ Snake/Assets/Scripts/Startup/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Init singletons
+		PlayerStats.Initialize();
+		PlayerStats.Instance.LoadStats ();
+
+		// transition to the next scene
+		SceneManager.LoadScene("Game", LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
