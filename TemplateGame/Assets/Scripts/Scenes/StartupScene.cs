9bf52354c7f4cdd4a37928e8e03e4cd2a8a5d918
Zarkwon <adamlis05@gmail.com>
2017-08-10 18:37:37 -0300

    Added tuning to tempaltegame

---
+++ TemplateGame/Assets/Scripts/Scenes/StartupScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cache;
+using SimpleJSON;
+
+public class StartupScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Tuning.Initialize ();
+		Tuning.Instance.LoadTuningFromDisk ();
+
+		JSONNode gameTuning = Tuning.Instance.GetJSON ("game");
+
+		Debug.Log ("Tuning value: " + gameTuning ["Test"].AsInt);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
